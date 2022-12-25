238580fa7383b06f047b96d10d6c321e30fe48bc
Filipe Alves <filipe.alvesrr@hotmail.com>
2022-12-25 20:34:10 -0300

    Adição do app de receitas

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# HTTP REQUEST
+
+
+def my_view(request):
+    return HttpResponse('Uma linda String')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
