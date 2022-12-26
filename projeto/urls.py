cc6e16eb61ba170e1db7968aee39e18382e5540a
Filipe Alves <filipe.alvesrr@hotmail.com>
2022-12-26 01:10:36 -0300

    Adição dos templates

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,17 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import include, path
 
 # HTTP REQUEST
 
-
-def my_view(request):
-    return HttpResponse('Uma linda String')
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('', include('recipes.urls')),
 ]
