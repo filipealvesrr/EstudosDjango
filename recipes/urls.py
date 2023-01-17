caa1b23cfcddde66a9ca2e5e64a89f48e460049d
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-01-17 15:18:44 -0300

    Ajustes de Css static e criação da page ecipe-view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from recipes.views import home
+# from recipes.views import home
+# from recipes import views
+# Algumas alternativas acima
+from . import views  # Foi usado o ponto por estar na pasta recipes
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    # o argumento <id>, informa que na view recipe deve ser
+    # passado um id como parametro da função.
+    path('recipes/<int:id>/', views.recipe)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Filipe Alves',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Filipe Alves',
+    })
