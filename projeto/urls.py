31df969aaf342989dbf8776e88bf8cd76d7309e6
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-01-29 23:13:43 -0300

    Ajustes arquivos de media e models

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 # HTTP REQUEST
 
@@ -22,3 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
